d7c9c311281e719cc93b29e03e21cbfba7471a52
musa <musa.talluzi@gmail.com>
2018-05-19 13:37:36 -0400

    add map to landing page

--- js/main.js
+++ js/main.js
@@ -9,3 +9,50 @@ $(document).ready(function(){
  })
 
 })
+
+var mymap = L.map('map').setView([43.6532, -79.3832], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	id: 'mapbox.streets'
+}).addTo(mymap);
+
+
+var greenIcon = L.icon({
+    iconUrl: 'images/leaf-green.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+L.marker([43.662069, -79.394250], {icon: greenIcon}).addTo(mymap)
+	.bindPopup("<b>DCSIL cool shool</b><br />Where founders are made.").openPopup();
+
+L.marker([43.64, -79.3832], {icon: greenIcon}).addTo(mymap)
+	.bindPopup("<b>Green School</b><br />Make the world a better place!").openPopup();
+
+
+L.circle([43.67, -79.38], 500, {
+	color: 'red',
+	fillColor: '#f03',
+	fillOpacity: 0.5
+}).addTo(mymap).bindPopup("Food desert area.");
+
+
+// var popup = L.popup();
+
+// function onMapClick(e) {
+// 	popup
+// 		.setLatLng(e.latlng)
+// 		.setContent("You clicked the map at " + e.latlng.toString())
+// 		.openOn(mymap);
+// }
+
+// mymap.on('click', onMapClick);
+
